8e811b74af48960bfb7253e477939220843b0246
Clinton Montenegro <montenegroclinton@yahoo.com>
2024-02-14 14:09:49 -0600

    exercise four complete

--- exercises/e4.js
+++ exercises/e4.js
@@ -4,12 +4,22 @@
 // findMaxValueInArray([2, 45, 32, 3, 0, 46, 12]) => 46
 
 export function findMinValueInArray(array) {
-  // Your code goes here...
+  for (let i = 0; i < array.length; i++) {
+    var minValue = array.reduce((a, b) => Math.min(a, b), Infinity);
+  }
+     return minValue;
 }
 
 export function findMaxValueInArray(array) {
-  // Your code goes here...
+    for (let i = 0; i < array.length; i++) {
+     var maxValue = array.reduce((a, b) => Math.max(a, b), -Infinity);
+    }
+        return maxValue;
 }
+
+console.log(findMinValueInArray([2, 45, 32, 3, 0, 46, 12]));
+console.log(findMaxValueInArray([2, 45, 32, 3, 0, 46, 12]));
+
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-4"
